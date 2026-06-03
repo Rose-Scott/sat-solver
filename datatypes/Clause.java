dbdf142e877e4771158200af4838ed154b1d7c17
Rose Scott <joshuascott7408@gmail.com>
2026-06-03 09:42:20 -0400

    clause class checks the status of itself in being satisfied

--- datatypes/Clause.java
+++ datatypes/Clause.java
@@ -9,16 +9,33 @@ public class Clause {
     }
 
 
+    public TruthValue satisfied(TruthValue[] variableValues) {
+        //loop through literals and determin
+        //satisfied, false or incomplete
+        boolean unsatisfied = false;
+        for (Literal lit : literals) {
+            if (variableValues[lit.variableId] == TruthValue.UNASSIGNED) {
+                unsatisfied = true;
+                continue;
+            }
+            // A literal is satisfied if its truth value XOR with negation is true
+            if (variableValues[lit.variableId].isTrue() ^ lit.isNegated) {
+                return TruthValue.TRUE;
+            }
+        }
+        return unsatisfied ? TruthValue.UNASSIGNED : TruthValue.FALSE;
+    }
+
     @Override
     public String toString()    {
         String clause = "";
         clause += "(";
         for (Literal lit : literals)    {
             clause += lit.toString();
-            clause += " V";
+            clause += " V ";
         }
 
-        clause = clause.substring(0, clause.length() - 2);
+        clause = clause.substring(0, clause.length() - 3);
         clause += ")";
         return clause;
     }
