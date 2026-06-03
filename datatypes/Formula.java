4e7e28cad83716be97616a147ae021f4fb7fb685
Rose Scott <joshuascott7408@gmail.com>
2026-06-03 14:56:59 -0400

    Formula verifies its own satisfied status

--- datatypes/Formula.java
+++ datatypes/Formula.java
@@ -8,16 +8,30 @@ public class Formula {
         this.clauses = clauses;
     }
 
+
+
+    public TruthValue satisfied(TruthValue[] variableValues)    {
+        boolean incomplete = false;
+        for (Clause clause : clauses) {
+
+            TruthValue clauseSat = clause.satisfied(variableValues);
+            if (clauseSat == TruthValue.UNASSIGNED) {incomplete = true;}
+            if (clauseSat == TruthValue.FALSE) {return TruthValue.FALSE;}
+
+        }
+        return incomplete ? TruthValue.UNASSIGNED : TruthValue.TRUE;
+        
+    }
     @Override
     public String toString()    {
         String formula = "";
 
         for (Clause cls: clauses)   {
             formula += cls.toString();
-            formula += " ^";
+            formula += " ^ ";
         }
 
-        formula = formula.substring(0,formula.length() - 2);
+        formula = formula.substring(0,formula.length() - 3);
         return formula;
     }
     
